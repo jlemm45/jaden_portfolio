262719adf2656933c6bfb64974918509908f8d02
Jaden Lemmon <jaden@jadenlemmon.com>
2021-03-05 10:35:44 -0600

    fix: Links

--- public/site.js
+++ public/site.js
@@ -1,11 +1,11 @@
-(function() {
+(function () {
   console.warn(
-    "Hey! Want to see the src? Check out github https://github.com/jlemm45/jaden"
+    'Hey! Want to see the src? Check out github https://github.com/jadenlemmon/jadenlemmon.com'
   );
-  $(window).on("load resize", function() {
-    $("#hero").height(window.innerHeight);
+  $(window).on('load resize', function () {
+    $('#hero').height(window.innerHeight);
 
-    var $el = $("#text-contain");
+    var $el = $('#text-contain');
     if (window.innerWidth <= 640 || window.innerHeight <= 675) {
       var elHeight = $el.outerHeight();
       var elWidth = $el.outerWidth();
@@ -19,37 +19,36 @@
 
       $el.css({
         transform:
-          "translate(" +
+          'translate(' +
           -60 * (1 - scale) * (1 - scale) +
-          "%, 20%) scale(" +
+          '%, 20%) scale(' +
           scale +
-          ")"
+          ')',
       });
     } else {
       $el.css({
-        transform: "scale(1)"
+        transform: 'scale(1)',
       });
     }
   });
 
-  $(document).on('click', 'a[href]', function(event) {
+  $(document).on('click', 'a[href]', function (event) {
     if (
-      location.pathname.replace(/^\//, "") ==
-        this.pathname.replace(/^\//, "") &&
+      location.pathname.replace(/^\//, '') ==
+        this.pathname.replace(/^\//, '') &&
       location.hostname == this.hostname
     ) {
       var target = $(this.hash);
-      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
-        $("html,body").animate(
+        $('html,body').animate(
           {
-            scrollTop: target.offset().top
+            scrollTop: target.offset().top,
           },
           1000
         );
         return false;
       }
     }
-  })
-
+  });
 })();
